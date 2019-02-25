67b793b9909ebe3132c5d9c9da3194c4a24dde0e
Тамила Мукушева <@local.itstep>
2019-02-25 17:45:49 +0600

    Добавьте файлы проекта.

---
+++ Accounting_Computers/Model/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting_Computers.Model
+{
+    enum TypeEquipmen { printer, monitor}
+
+    public struct Equipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public TypeEquipmen typeEquipment T{ get; set; }
+
+    }
+}
---
+++ Accounting_Computers/Model/ServiceEquipment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Accounting_Computers.Model
+{
+   public struct ServiceEquipment
+    {
+        private delegate void dException(string message);
+        public delegate void dSuccessfull(Equipment equipment);
+
+        private dException DException;
+        private dSuccessfull DSuccessfull;
+        public void RegisterDelegate(dException DException)
+        {
+            this.DException = DException;
+            this.DSuccessfull = DSuccessfull;
+
+        }
+        public void AddEquipment(Equipment equipment)
+        {
+            try
+            {
+                using (var db = new LiteDatabase("Accounting.db"))
+
+                {
+                    var equipments = db.GetCollection<Equipment>("Equipment");
+                    equipments.Insert(equipment);
+
+
+                }
+                if (DSuccessfull != null)
+                    DSuccessfull.Invoke(equipment);
+    
+            
+            }
+            catch(Exception ex)
+            {
+                if (DException != null)
+                    DException.Invoke(ex.Message);
+            }
+        }
+    }
+}
